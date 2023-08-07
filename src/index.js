edae847d8afe85d6720eb18bafd50be7cfcf9381
Igor <igorkuznecov@gmx.de>
2023-08-07 16:12:46 +0200

    feat: mission completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const string = n.toString().replace(/-/g, "");
+    let result = [];
+    for (let i = 0; i < string.length; i++) {
+        result.push(string[string.length - 1 - i]);
+    }
+    return result.toString().replace(/,/g, "");
+};
